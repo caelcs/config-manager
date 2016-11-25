f2624c67211a7d02b429574fe6b6a199131f381b
aer01 <adolfo.ernestocustidianosecchi@sky.uk>
2016-11-25 16:21:39 +0000

    wip1

--- src/main/java/uk/co/caeldev/config/manager/api/buildConfig/BuildConfigController.java
+++ src/main/java/uk/co/caeldev/config/manager/api/buildConfig/BuildConfigController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:8888", maxAge = 3600)
+@CrossOrigin(origins = {"*"}, maxAge = 3600)
 @RestController
 public class BuildConfigController {
 
